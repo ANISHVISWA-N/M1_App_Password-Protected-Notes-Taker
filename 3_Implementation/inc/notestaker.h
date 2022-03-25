372a3157e628b45116e32d3520eedd7e55fafb71
ANISHVISWA-N <101496213+ANISHVISWA-N@users.noreply.github.com>
2022-03-25 17:33:20 +0530

    Add files via upload

---
+++ 3_Implementation/inc/notestaker.h
@@ -0,0 +1,20 @@
+#ifndef NOTESTAKER_H_INCLUDED
+#define NOTESTAKER_H_INCLUDED
+struct notes
+
+{
+
+    char time[6];
+
+    char name[30];
+
+    char place[25];
+
+    char duration[10];
+
+    char note[500];
+
+} ;
+
+
+#endif // NOTESTAKER_H_INCLUDED
